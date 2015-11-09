5c9a0c8d7c0a6c8d8716d7a6e3cd91ad72f75a0f
atanikov <atanikov@gmail.com>
2015-11-09 18:26:50 +0300

    исправлено написание слова Search в переводе поиска в шапке - с gelikon-shop.com

--- themes/Gelikon/modules/blocksearch/translations/en.php
+++ themes/Gelikon/modules/blocksearch/translations/en.php
@@ -9,6 +9,6 @@ $_MODULE['<{blocksearch}gelikon>blocksearch_be305c865235f417d9b4d22fcdf9f1c5'] =
 $_MODULE['<{blocksearch}gelikon>blocksearch_13348442cc6a27032d2b4aa28b75a5d3'] = 'Search';
 $_MODULE['<{blocksearch}gelikon>blocksearch_ce1b00a24b52e74de46971b174d2aaa6'] = 'Search products:';
 $_MODULE['<{blocksearch}gelikon>blocksearch_5f075ae3e1f9d0382bb8c4632991f96f'] = 'Go';
-$_MODULE['<{blocksearch}gelikon>blocksearch-footer_83f37574e2ea275acfb3660549796277'] = 'Serach';
+$_MODULE['<{blocksearch}gelikon>blocksearch-footer_83f37574e2ea275acfb3660549796277'] = 'Search';
 $_MODULE['<{blocksearch}gelikon>blocksearch-footer_53660e081bed47bc53e7d4d247f7b15d'] = 'All categories';
 $_MODULE['<{blocksearch}gelikon>blocksearch-top_53660e081bed47bc53e7d4d247f7b15d'] = 'All categories';
